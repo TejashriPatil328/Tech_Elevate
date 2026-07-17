410abe3e4e3d03b3d32789b3740cf93ce6b658d5
Tejashri Patil <your-github-tejashrip325@gmail.com>
2026-07-17 16:21:05 +0530

    Debug VITE_API_URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+  withCredentials: true,
 });
 
 export default axiosInstance;
