706e9bc831e59bf882294461f166c41361c7aba1
Tejashri Patil <your-github-tejashrip325@gmail.com>
2026-07-17 16:10:51 +0530

    Fix Clerk authentication

--- frontend/src/hooks/useSyncUser.js
+++ frontend/src/hooks/useSyncUser.js
@@ -1,20 +1,23 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 import { useEffect } from "react";
-import { useUser } from "@clerk/clerk-react";
+import { useUser, useAuth } from "@clerk/clerk-react";
 
 const useSyncUser = () => {
   const { user, isLoaded, isSignedIn } = useUser();
-
+  const { getToken } = useAuth();
   useEffect(() => {
     if (!isLoaded || !isSignedIn || !user) return;
 
     const sync = async () => {
       try {
+        const token = await getToken();
+
         await fetch(`${API_URL}/users/sync`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
             clerkId: user.id,
@@ -29,7 +32,7 @@ const useSyncUser = () => {
     };
 
     sync();
-  }, [isLoaded, isSignedIn, user]);
+  }, [isLoaded, isSignedIn, user, getToken]);
 };
 
 export default useSyncUser;
